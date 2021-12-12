790314b0c2c6ffca044d7d01548e93fa117eea03
AhmedOsama93 <75946833+AhmedOsama93@users.noreply.github.com>
2021-12-12 22:44:03 +0200

    Add files via upload

---
+++ indexFile&secondaryFile/cmake-build-debug/CMakeFiles/assgin1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assgin1.dir/main.cpp.obj"
+  "assgin1.exe"
+  "assgin1.exe.manifest"
+  "assgin1.pdb"
+  "libassgin1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assgin1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ indexFile&secondaryFile/cmake-build-debug/CMakeFiles/assgin1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for assgin1.
+# This may be replaced when dependencies are built.
